c133eaf48f555c0df84a5eea7e92d4a3d70e260f
sebascelis03 <sebascelis03@gmail.com>
2025-10-06 22:29:35 -0500

    parte 1 interfaz

---
+++ main.py
@@ -0,0 +1,8 @@
+from app.menu import mostrar_menu_principal
+
+def main():
+    print("=== CALCULADORA DE INTEGRALES MÚLTIPLES ===\n")
+    mostrar_menu_principal()
+
+if __name__ == "__main__":
+    main()
